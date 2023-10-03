5d174f96eeadce279bbfbc7c9077a2e6ecf7d3a9
Le Nguyen Vu <nguyenvu@Les-MacBook-Pro.local>
2023-10-03 20:28:55 +0700

    lab_02_1

---
+++ sdetpro-k12-java/src/lab_02/lab_02_1.java
@@ -0,0 +1,28 @@
+package lab_02;
+
+import java.util.Scanner;
+
+public class lab_02_1 {
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your height(m): ");
+        double userHeight = scanner.nextDouble();
+        System.out.println("Please input your weight(kg): ");
+        double userWeight = scanner.nextDouble();
+
+        double userBMI;
+        userBMI = userWeight/(userHeight*2);
+
+        if (userBMI <= 18.5) {
+            System.out.println("Underweight");
+        } else if (userBMI <= 24.9) {
+            System.out.println("Normal weight");
+        } else if (userBMI <= 29.9) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obesity");
+        }
+
+    }
+}
