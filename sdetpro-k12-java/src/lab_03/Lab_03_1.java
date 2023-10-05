617fba8b70ad135cf2dacff5ed26ff0a5c214df2
Le Nguyen Vu <nguyenvu@Les-MacBook-Pro.local>
2023-10-05 14:45:17 +0700

    lab_03_1

---
+++ sdetpro-k12-java/src/lab_03/Lab_03_1.java
@@ -0,0 +1,23 @@
+package lab_03;
+
+import java.util.Arrays;
+
+public class Lab_03_1 {
+    public static void main (String[] args) {
+        int[] intArr = {1, 2, 3, 4, 5};
+        int arrayLength = intArr.length;
+        int evenNumber = 0;
+        int oddNumber = 0;
+
+        for (int i = 0; i < arrayLength; i++) {
+            if (intArr[i]%2 == 0){
+                evenNumber++;
+            } else {
+                oddNumber++;
+            }
+        }
+        System.out.println("The array: " + Arrays.toString(intArr));
+        System.out.println("Number of Even number is : " + evenNumber);
+        System.out.println("Number of Odd number is : " + oddNumber);
+    }
+}
